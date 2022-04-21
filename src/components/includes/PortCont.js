c37c7cf017b168a70c147ed2b23d6178408a4a4c
ohwehoh <ohwehoh@naver.com>
2022-04-21 10:21:11 +0900

    react20220421

---
+++ src/components/includes/PortCont.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+//함수형 컨퍼넌트 ++클래스 컨퍼넌트-->리액트 훅
+
+function PortInfo({image, title, category, link, id}){
+    return (
+        <article className="port__item">
+            <figure className="img">
+                <a href={link}><img src={image} alt={`port0${id}`} /></a>
+            </figure>
+            <div className="text">
+                <h3>{title}</h3>
+                <p>{category}</p>
+            </div>
+        </article>
+    )
+}
+
+// const portText = [
+//     {
+//         image : "/img/port01@2x.jpg",
+//         alt : "port1",
+//         title : "Portfolio Title",
+//         desc : "Portfolio"
+//     },
+//     {
+//         image : "/img/port01@2x.jpg",
+//         alt : "port1",
+//         title : "Portfolio Title",
+//         desc : "Portfolio"
+//     },
+//     {
+//         image : "/img/port01@2x.jpg",
+//         alt : "port1",
+//         title : "Portfolio Title",
+//         desc : "Portfolio"
+//     },
+// ]
+
+function PortCont({port}){
+    return(
+        <section className="port__cont">
+            <div className="container">
+                <div className="port__inner">
+                    {port.map((txt) => (
+                        <PortInfo
+                            key = {txt.id}
+                            image = {txt.image}
+                            title = {txt.title}
+                            category = {txt.category}
+                            link = {txt.link}
+                            id = {txt.id}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default PortCont;
--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -1,21 +1,125 @@
 import React from 'react';
-import Header from "../includes/Header";
-import Contents from "../includes/Contents";
-import Footer from "../includes/Footer"
+import Header from "../layout/Header";
+import Contents from "../layout/Contents";
+import ContTitle from '../layout/ContTitle';
+import ReferenceCont from '../includes/ReferenceCont';
+import Contact from '../layout/Contact';
+import Footer from "../layout/Footer";
+import Loading from "../basics/Loading";
+import { gsap } from "gsap";
+import axios from 'axios';
 
-function Reference(){
-    return (
-        <>
-            <Header />
-            <Contents>
-                <section className="reference__cont">
-                    <div className="reference__inner">
-                        <div>reference</div>
-                    </div>
-                </section>
-            </Contents>
-            <Footer />
-        </>
-    )
+
+// function Reference(){
+//     return (
+//         <>
+//             <Header color="light"/>
+//             <Contents color="light">
+//                 <ContTitle title={["reference","book"]} color="light"/>
+//                 <ReferenceCont />
+//             </Contents>
+//             <Contact/>
+//             <Footer color="light"/>
+//         </>
+//     )
+// }
+
+class Reference extends React.Component{
+    state = {
+        isLoading: true,
+        refers: []
+    }
+
+    mainAnimation = () => {
+        setTimeout(() => {
+            gsap.to("#header", {
+                duration: 0.8,
+                top: 0,
+            });
+            gsap.to("#footer", {
+                duration: 0.8,
+                bottom: 0,
+                delay: 0.2,
+            });
+            gsap.to(".cont__title strong", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.0,
+                ease: "power4.out"
+            });
+            gsap.to(".cont__title em", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.3,
+                ease: "power4.out"
+            });
+            gsap.to(".refer__cont", {
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
+                delay: 1.5,
+                ease: "power4.out"
+            });
+        }, 10)
+    }
+
+    getSite = async () => {
+        const {
+            data: {
+                data: {refer},
+            },
+        } = await axios.get("https://ohwehoh.github.io/web_class_react/react2022/src/assets/json/reference.json");
+
+        setTimeout(() => {
+            this.setState({refers:refer, isLoading:false});
+            this.mainAnimation();
+        }, 1600)
+        console.log(refer);
+    }
+
+    // componentDidMount(){
+    //     setTimeout(() => {
+    //         document.getElementById("loading").classList.remove("loading__active");
+    //         document.querySelector("body").style.background = "#f0eeeb";
+    //     }, 1000)
+        
+    //     setTimeout(() => {
+    //         // this.mainAnimation();...로딩이 다 안돼서 .main__inner를 불러오지 못한다.
+    //         this.getSite();
+    //     }, 3000)
+    // }
+
+    componentDidMount(){
+        document.querySelector("body").style.background = "#F0EEEB";
+        setTimeout(() => {
+            document.getElementById("loading").classList.remove("loading__active");
+            this.getSite();
+        }, 2000);
+    }
+
+    render(){
+        const {isLoading, refers} = this.state;
+        console.log("1" + isLoading);
+        console.log(refers);
+        return (
+            <>
+                {isLoading ? (
+                    <Loading color="light"/>
+                ) : (
+                    <>
+                        <Header color="light"/>
+                        <Contents color="light">
+                            <ContTitle title={["reference","book"]} color="light"/>
+                            <ReferenceCont refer={refers} />
+                            <Contact/>
+                        </Contents>
+                        <Footer color="light"/>
+                    </>
+                )}
+            </>
+        )
+    }
 }
 export default Reference;
