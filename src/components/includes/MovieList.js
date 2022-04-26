704a1d2408e0cca08480b2b1fef86b68ffdfc1db
ohwehoh <ohwehoh@naver.com>
2022-04-26 18:48:47 +0900

    0426

--- src/components/includes/MovieList.js
+++ src/components/includes/MovieList.js
@@ -3,7 +3,7 @@ import React from 'react'
 function MovieItem(props){
     return (
         <li className='movieList'>
-            <a href={`https://www.themoviedb.org/movie/${props.item.id}-${props.item.title}`} target='_blank'>
+            <a href={`https://www.themoviedb.org/movie/${props.item.id}-${props.item.title}`}>
             {props.item.poster_path ?
                 <div className="listWrap">
                     <img src={`https://image.tmdb.org/t/p/w500/${props.item.poster_path}`} alt={props.item.original_title} />
